8d6d1fd179cc44af35ec820493023707f1c3874a
Isaac Kim <isaacyunkim@gmail.com>
2025-05-05 12:59:37 -0700

    created test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import puppy 
+
+
+def main(): 
+    print("Puppy State Tester")
+    pup = puppy.Puppy()
+
+    option = 0 
+    while option != 3: 
+        option = int(input("1. Feed\n2. Play\n3. Exit\n"))
+
+
+
+main()
