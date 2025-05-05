9db035e180958ab97b21a066fd81f526c50c078b
Isaac Kim <isaacyunkim@gmail.com>
2025-05-05 12:54:22 -0700

    added methods

---
+++ puppy.py
@@ -0,0 +1,40 @@
+
+import puppy_state
+import state_asleep
+import state_play
+import state_eat
+
+class Puppy: 
+
+
+    def __init__(self): 
+        self._state = state_asleep.StateAsleep()
+        self._feeds = 0 
+        self._plays = 0 
+
+    @property
+    def _feed(self): 
+        return self._feeds
+
+    @property
+    def _play(self): 
+        return self._plays
+
+    def change_state(self, new_state): 
+        self._state = new_state
+
+    def throw_ball(self):
+        self._state.play() 
+    
+    def give_food(self): 
+        self._state.feed()
+
+    def inc_feeds(self): 
+        self._feeds += 1
+
+    def inc_plays(self):
+        self._plays += 1    
+    
+    def reset(self): 
+        self._feeds = 0
+        self._plays = 0
